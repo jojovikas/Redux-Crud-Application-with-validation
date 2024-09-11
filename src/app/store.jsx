32322b023aa0d97dcf0ec11373a64133102ee03c
jojovikas <vikasfrontenddeveloper007@gmail.com>
2024-09-11 16:43:39 +0530

    new code push

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -3,6 +3,7 @@ import userSlice from "./user/userSlice";
 
 // checking git code push or not
 // it is 2nd time tocheck data push or not
+// 2rd time push the code
 export const store = configureStore({
   reducer: {
     user: userSlice,
