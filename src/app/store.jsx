6fa83ead7de497897dd445c7c8afcc4de7301eda
jojovikas <vikasfrontenddeveloper007@gmail.com>
2024-09-11 16:38:24 +0530

    push code

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user/userSlice";
 
+// checking git code push or not
 export const store = configureStore({
-    reducer: {
-        user: userSlice
-    }
+  reducer: {
+    user: userSlice,
+  },
 });
-
 
 export default store;
