97a494181eeaa448ec14fc483574dcae71c879ba
jojovikas <vikasfrontenddeveloper007@gmail.com>
2024-09-11 16:41:53 +0530

    data send

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user/userSlice";
 
 // checking git code push or not
+// it is 2nd time tocheck data push or not
 export const store = configureStore({
   reducer: {
     user: userSlice,
